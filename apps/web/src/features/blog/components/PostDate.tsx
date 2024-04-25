17551350083ddfe2c09105c7edfbc8e57416865d
atman-33 <gpbjk0304@gmail.com>
2024-04-25 23:01:58 +0900

    style: minor fix

--- apps/web/src/features/blog/components/PostDate.tsx
+++ apps/web/src/features/blog/components/PostDate.tsx
@@ -4,7 +4,7 @@ import { Post } from 'contentlayer/generated';
 
 const PostDate = ({ post }: { post: Post }) => {
   return (
-    <div className="flex items-center space-x-2 text-sm text-slate-500">
+    <div className="flex items-center space-x-2 text-xs text-slate-500">
       <div className="flex items-center">
         <Icons.edit className="h-5 w-5" />
         <span>{getFormattedDate(new Date(post.date), 'yyyy/MM/dd')}</span>
