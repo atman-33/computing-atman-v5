2c944265341c74c5b80ab8fb70630186759a4160
atman-33 <gpbjk0304@gmail.com>
2024-05-02 13:16:21 +0900

    feat: sitemap に、tech react 記事を追加

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,18 +1,25 @@
 import { siteConfig } from '@/config/site-config';
+import { reactDocsConfig } from '@/config/tech/react-docs-config';
 import { allPosts } from 'contentlayer/generated';
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteConfig.url;
+
+  const routes = ['', 'blog/tags', 'tech', 'tech/react'].map((route) => ({
+    url: `${siteUrl}/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
   const blogRoutes = allPosts.map((post) => ({
     url: `${siteUrl}/${post.slug}`,
     lastModified: post.lastmod || post.date,
   }));
 
-  const routes = ['', 'blog/tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
+  const techReactRoutes = reactDocsConfig.sidebarNav[0]?.items.map((item) => ({
+    url: `${siteUrl}/${item.slug}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...blogRoutes];
+  return [...routes, ...blogRoutes, ...(techReactRoutes ?? [])];
 }
