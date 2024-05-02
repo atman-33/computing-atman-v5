4e085f3ba342bd8f13c382e6f45c5e16307c64b0
atman-33 <gpbjk0304@gmail.com>
2024-05-02 15:04:37 +0900

    feat: minor commit

--- apps/web/src/app/(app)/tech/page.tsx
+++ apps/web/src/app/(app)/tech/page.tsx
@@ -2,11 +2,9 @@ import { TechList } from '@/features/tech';
 
 const TechPage = () => {
   return (
-    <div className="flex justify-center">
-      <div className="my-4 flex flex-col space-y-4">
-        <h3 className="ml-3 self-center text-xl font-bold">Tech</h3>
-        <TechList />
-      </div>
+    <div className="my-8 flex flex-col space-y-4">
+      <h3 className="self-center text-xl font-bold">Tech Articles</h3>
+      <TechList />
     </div>
   );
 };
--- apps/web/src/features/tech/components/TechList.tsx
+++ apps/web/src/features/tech/components/TechList.tsx
@@ -6,7 +6,8 @@ export const TechList = () => {
   return (
     <div className="mt-8 flex flex-col space-y-4">
       {techConfig.map((tech) => (
-        <div key={tech.title} className="hover:bg-primary/10 rounded-md p-4">
+        <div key={tech.title} className="hover:bg-primary/10 rounded-md md:p-4">
+          <hr className="my-2" />
           <Link href={tech.href}>
             <div className="flex items-center space-x-8">
               <Icon kind={tech.icon as IconKind} size={24} />
