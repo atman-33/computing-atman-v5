25164229cb2b22d718b63d17b7a640d4c602c6b3
atman-33 <gpbjk0304@gmail.com>
2024-05-05 21:11:22 +0900

    fix: 画面レイアウト見直し

--- apps/web/src/app/(app)/app/page.tsx
+++ apps/web/src/app/(app)/app/page.tsx
@@ -1,9 +1,11 @@
 import { AppList } from '@/features/app';
+import { Separator } from '@repo/ui';
 
 const AppPage = () => {
   return (
     <div className="my-8 flex flex-col space-y-4">
       <h3 className="self-center text-xl font-bold">Wep Applications</h3>
+      <Separator orientation="horizontal" />
       <AppList />
     </div>
   );
