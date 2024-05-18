34f2e4409b349e68c265c6e847b0ae89dbe80fde
atman-33 <gpbjk0304@gmail.com>
2024-05-18 21:42:34 +0900

    refactor(web): blog用のjsonファイルをsrc内に配置するよう見直し

--- apps/web/src/app/(__debug__)/debug/005-fetch/page.tsx
+++ apps/web/src/app/(__debug__)/debug/005-fetch/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 const Page = () => {
   useEffect(() => {
     const getSearchData = async () => {
-      const response = await fetch('/search-data.json');
+      const response = await fetch('/site.webmanifest');
       const data = await response.json();
       console.log(data);
     };
---
+++ apps/web/src/config/@generated/README.md
@@ -0,0 +1,4 @@
+# フォルダ内のファイルについて補足
+
+- search-data.json: contentlayer の処理中に生成。contentlayer.config.ts にて定義
+- tag-data.json: contentlayer の処理中に生成。contentlayer.config.ts にて定義
